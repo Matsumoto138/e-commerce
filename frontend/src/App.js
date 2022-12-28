300bb66606ffa375d82ef2ad67395db868e0e925
Fatih Demirbas <dmrbs.fd@gmail.com>
2022-12-28 18:05:17 +0300

    First Commit

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,14 +1,31 @@
-import logo from './logo.svg';
 import './App.css';
+import data from './data'
 
 function App() {
   return (
     <div className="App">
       <header>
-        <a href="/">C-Store</a>
+        <a href="/">Tech Store</a>
       </header>
       <main>
-        list products
+        <h1>Featured Product</h1>
+        <div className="products">
+        {
+          data.products.map((product) =>
+            (<div key={product.slug} className="product">
+              <a href={`/product/${product.slug}`}>
+              <img style={{maxWidth:'300px'}} src={product.image} alt={product.name} />
+              </a>
+              <div className="product-info">
+              <a href={`/product/${product.slug}`}>
+                <p>{product.name}</p>
+              </a>
+                <p><strong>{product.price}₺</strong></p>
+              </div>
+            </div>)
+          )
+        }
+        </div>
       </main>
     </div>
   );
---
+++ frontend/src/data.js
@@ -0,0 +1,54 @@
+const data = {
+    products: [
+        {
+            name: 'I-Phone 13 Pro',
+            slug: 'iphone-13-pro',
+            category: 'Phones',
+            image: '/images/product1.png', // 679px × 829px
+            price: 20.000,
+            countInStock: 10,
+            brand: 'Apple',
+            rating: 4.5,
+            numReviews: 10,
+            description: 'high quality phone',
+          },
+          {
+            name: 'HP Laptop',
+            slug: 'hp-lapto',
+            category: 'Laptops',
+            image: '/images/2.png',
+            price: 8.000,
+            countInStock: 20,
+            brand: 'HP',
+            rating: 4.0,
+            numReviews: 10,
+            description: 'high quality product',
+          },
+          {
+            name: 'Canon Camera',
+            slug: 'canon-camera',
+            category: 'Cameras',
+            image: '/images/3.png',
+            price: 25,
+            countInStock: 15,
+            brand: 'Canon',
+            rating: 4.5,
+            numReviews: 14,
+            description: 'high quality product',
+          },
+          {
+            name: 'Adidas Fit Jacket',
+            slug: 'adidas-fit-jacket',
+            category: 'Clothes',
+            image: '/images/4.png',
+            price: 65,
+            countInStock: 5,
+            brand: 'Adidas',
+            rating: 4.5,
+            numReviews: 10,
+            description: 'high quality product',
+          },
+    ]
+}
+
+export default data;
