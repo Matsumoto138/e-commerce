749ac9bec5364a4d8ee9e44ac99e5503aba6156a
Fatih Demirbas <dmrbs.fd@gmail.com>
2022-12-28 17:08:26 +0300

    Create-React-App

---
+++ frontend/src/App.js
@@ -0,0 +1,17 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header>
+        <a href="/">C-Store</a>
+      </header>
+      <main>
+        list products
+      </main>
+    </div>
+  );
+}
+
+export default App;
